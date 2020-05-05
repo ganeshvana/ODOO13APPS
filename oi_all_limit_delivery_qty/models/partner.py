a20c5e55397b76c70b0f66ba4d2152c177243ab9
ramya-itara <ramya@itarait.com>
2020-05-05 10:31:49 +0530

    05052020_1031

---
+++ oi_all_limit_delivery_qty/models/partner.py
@@ -0,0 +1,17 @@
+from odoo import api, fields, models, _
+
+class res_partner(models.Model):
+    _inherit = "res.partner"
+
+    credit_limit = fields.Integer(string="Credit Limit")
+    credit_limit_applicable = fields.Boolean("Credit Limit Applicable")
+
+    date_credit_limit = fields.Integer(string="Days Limit")
+    date_credit_limit_applicable = fields.Boolean(" Days Expiry Applicable")
+
+    invoice_credit_limit = fields.Integer(string="Invoice Limit")
+    invoice_credit_limit_applicable = fields.Boolean("Invoice Limit Applicable")
+
+
+
+

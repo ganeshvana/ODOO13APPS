19c849c7d41344f43bbf205fcd3a8e6a627c3e65
ramya-itara <ramya@itarait.com>
2020-05-18 21:08:31 +0530

    18052020

--- oi_credit_limit_delivered_qty_override/models/stock_picking.py
+++ oi_credit_limit_delivered_qty_override/models/stock_picking.py
@@ -46,14 +46,14 @@ class Picking(models.Model):
             if self.partner_id.credit_limit_applicable and self.partner_id.credit_limit and not self.override_credit_limit:
                 if exceed_amount > self.partner_id.credit_limit :
                         raise UserError(_('Credit limit exceeded for this customer'))
-        # sale = self.env['sale.order'].search([('partner_id','=', self.partner_id.id),('state','not in',['draft','cancel'])])
-        # for sales_cou in sale:
-        #     sale_total+= sales_cou.amount_total
-        #     cus_sale_amount = sale_total - payment_total
-        #     print ("Amountttttttt",cus_sale_amount,sale_total)
-        #     if not self.override_credit_limit and self.partner_id.credit_limit and self.partner_id.credit_limit_applicable: 
-        #         if cus_sale_amount > self.partner_id.credit_limit:
-        #             raise UserError(_('Credit limit exceeded for this customer'))
+        sale = self.env['sale.order'].search([('partner_id','=', self.partner_id.id),('state','not in',['draft','cancel'])])
+        for sales_cou in sale:
+            sale_total+= sales_cou.amount_total
+            cus_sale_amount = sale_total - payment_total
+            print ("Amountttttttt",cus_sale_amount,sale_total)
+            if not self.override_credit_limit and self.partner_id.credit_limit and self.partner_id.credit_limit_applicable: 
+                if cus_sale_amount > self.partner_id.credit_limit:
+                    raise UserError(_('Credit limit exceeded for this customer'))
         if delivered_quantity:
             if exceed_amount >= self.partner_id.credit_limit:
                 if not self.override_credit_limit and self.partner_id.credit_limit_applicable and self.partner_id.credit_limit:
